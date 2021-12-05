e4c0ff382faf54506a629a4310e9bc3e7293ca7e
Derek Gan <187380048+derekgan08@users.noreply.github.com>
2021-12-05 21:07:59 +0800

    Create css and styles

---
+++ assets/style.php
@@ -0,0 +1,29 @@
+<?php
+header("Content-type: text/css; charset: UTF-8");
+?>
+
+
+.error{
+background: red;
+color: white;
+text-align: center;
+right: 10%;
+position: absolute;
+width: 500px;
+height: 30px;
+transform: translate(10%,10%);
+border-radius: 0 0 20px 20px;
+box-sizing: border-box;
+}
+
+.success{
+background: green;
+text-align: center;
+right:10%;
+position: absolute;
+width: 500px;
+height: 30px;
+transform: translate(10%,10%);
+border-radius: 0 0 20px 20px;
+}
+
